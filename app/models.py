e83fa0c8ba25a70f1a2952b8e992ddd43905306d
Nguyen Thanh Trieu <trieukon1011@gmail.com>
2024-11-21 17:55:47 +0700

    a

--- app/models.py
+++ app/models.py
@@ -1,5 +1,5 @@
 from sqlalchemy import Integer,String,Float,ForeignKey,Boolean,Column,DateTime,Enum,Text
-from hospitalapp import app,db
+from app import app,db
 from sqlalchemy.orm import relationship, mapped_column, Mapped, backref
 from enum import Enum as RoleEnum
 from datetime import datetime
