66ae1c776361ae324181f9087cc6b658567fa4a6
thereaper0602 <2251012121quang@ou.edu.vn>
2024-11-28 21:40:05 +0700

    new models

--- app/models.py
+++ app/models.py
@@ -18,7 +18,8 @@ class NguoiDung(db.Model): # Có nên là abstract class
     ho = Column(String(10),nullable=False,unique = True)
     ten = Column(String(10),nullable = False)
     ngaySinh = Column(DateTime,nullable = False)
-    soDienThoai = Column(String(15),nullable=False,unique = True)
+    soDienThoai = Column(String(15),nullable=True,unique = True)
+    email = Column(String(50),nullable=True,unique=True)
     ghiChu = Column(String(255),nullable = True)
     taiKhoan = Column(String(50),nullable = False,unique = True)
     matKhau = Column(Text,nullable = False)
@@ -28,9 +29,6 @@ class NguoiDung(db.Model): # Có nên là abstract class
 
     def __str__(self):
         return f"{self.ho} {self.ten}"
-
-    def check_password(self,password):
-        return True
 
 class QuyDinh(db.Model):
     id = Column(Integer,primary_key=True,autoincrement=True,nullable=False)
@@ -49,14 +47,6 @@ class HoaDonThanhToan(db.Model):
     # Bac Si
     phieuKham_id = Column(Integer, ForeignKey("phieuKham.id"), unique=True)
 
-phieuKham_DichVu = db.Table('phieuKham_DichVu',
-                            Column('phieuKham_id',Integer,
-                            ForeignKey('phieuKham.id'),
-                            primary_key = True),
-                            Column('dichVu_id',Integer,
-                            ForeignKey('dichVu.id'),
-                            primary_key = True))
-
 class PhieuKhamBenh(db.Model):
     __tablename__ = 'phieuKham'
     id = Column(Integer,primary_key=True,autoincrement=True,nullable=False)
@@ -65,22 +55,38 @@ class PhieuKhamBenh(db.Model):
     duDoanLoaiBenh = Column(String(255),nullable = False)
     bacSi_id = Column(Integer,ForeignKey(NguoiDung.id),nullable = False)
     benhNhan_id = Column(Integer,ForeignKey(NguoiDung.id),nullable = False)
-    dichvu = relationship('DichVuKham',
-                          secondary='phieuKham_DichVu',
-                          lazy = 'subquery',
-                          backref=backref('phieuKham',lazy = True))
+    phieuDichVu = relationship('PhieuDichVu',backref = backref('phieuKham',uselist = False)) 
     hoaDonThanhToan = relationship('HoaDonThanhToan', backref = backref('phieuKham',uselist = False))
 
-class DichVuKham(db.Model):
-    __tablename__ = 'dichVu'
+class PhieuDichVu(db.Model):
+    __tablename__ = 'phieudichvu'
     id = Column(Integer,primary_key = True,autoincrement=True)
-    tenDichVu = Column(String(100),nullable = False)
-    moTa = Column(String(255),nullable = True)
-    phi = Column(Float,nullable = False,default = 0.0)
+    ngayLap = Column(DateTime,default=datetime.utcnow)
+    ghiChu = Column(Text,nullable=True)
+    phieukham_id = Column(Integer,ForeignKey("phieuKham.id"),unique = True)
+
+class LoaiDichVu(db.Model):
+    __tablename__ = 'loaidichvu'
+    id = Column(Integer,primary_key = True,autoincrement=True)
+    tenDichVu = Column(String(255),nullable=False,unique=True)
+    giaDichVu = Column(Float,default=0,nullable=False)
+    moTa = Column(Text,nullable=True)
+
+chiTietDichVu = db.Table('chiTietDichVu',
+                         Column('phieuDichVu_id',
+                                Integer,
+                                ForeignKey('phieudichvu.id'),
+                                primary_key=True,
+                                ),
+                        Column('loaiDichVu',
+                               Integer,
+                               ForeignKey('loaidichvu.id'),
+                               primary_key=True),
+                        Column('ketQua',String(255),nullable=False))
 
 class DanhMucThuoc(db.Model):
     __tablename__ = 'danhMucThuoc'
-    id = Column(Integer,primary_key=True,autoincrement=True,nullable=False)
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     ten = Column(String(100),nullable = False,unique=True)
     thuoc = relationship('Thuoc',backref = 'danhMucThuoc',lazy = True)
 
@@ -116,7 +122,6 @@ class PhieuLichDat(db.Model):
     trangThai = Column(Boolean,nullable = False,default = False)
 
     nguoiDung_id = Column(Integer,ForeignKey(NguoiDung.id),nullable = False)
-
 
 if __name__ == '__main__':
     with app.app_context():
